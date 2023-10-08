3d873e85086539482e19bc6cdcea6ef841c33241
HShagroon <haneenshagroon@gmail.com>
2023-10-08 11:09:14 +0300

    Initial Commit

---
+++ team12/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  team12
+//
+//  Created by Haneen Rida Shagroon on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ team12/team12App.swift
@@ -0,0 +1,17 @@
+//
+//  team12App.swift
+//  team12
+//
+//  Created by Haneen Rida Shagroon on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct team12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
